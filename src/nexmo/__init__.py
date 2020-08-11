2624e201a9d4a3348f3c4307b5dd4da11aa9c383
Diana Rodriguez <61435963+superdiana@users.noreply.github.com>
2020-08-11 11:05:26 -0400

    fixing indentation

--- src/nexmo/__init__.py
+++ src/nexmo/__init__.py
@@ -290,10 +290,8 @@ class Client:
             DeprecationWarning,
             stacklevel=2,
         )
-
         return self.post(self.api_host(), "/verify/check/json", params or kwargs)
-
-    def start_psd2_verification_request(self, params=None, **kwargs)
+def start_psd2_verification_request(self, params=None, **kwargs)
         return self.post(
             self.api_host(), "/verify/psd2/json", params or kwargs
         )
